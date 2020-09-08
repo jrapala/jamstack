619cea6e7051b0eaaeddb963293768a295e8cfc4
Juliette Rapala <jrapala@gmail.com>
2020-09-08 16:09:52 -0500

    feat(dashboard): working protected routes

--- sections/dashboard/src/components/layout.js
+++ sections/dashboard/src/components/layout.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { IdentityContextProvider } from 'react-netlify-identity-widget'
 
 import './layout.css'
 
 const Layout = ({children}) => (
-	<>
+	<IdentityContextProvider url="https://juliettes-jamstack-auth-app.netlify.app">
 		<header>
 			<Link to="/">JAMstack App</Link>
 		</header>
 		<main>{children}</main>
-	</>
+	</IdentityContextProvider>
 )
 
 export default Layout
