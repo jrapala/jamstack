e14010ec7652ae29f75863433b19487cc9772b3a
Juliette Rapala <jrapala@gmail.com>
2020-09-08 12:39:27 -0500

    feat(contact): working contact form

--- sections/contact/functions/contact.js
+++ sections/contact/functions/contact.js
@@ -1,10 +1,26 @@
+require('dotenv').config()
+
 // The callback returns the status code
 exports.handler = (event, _context, callback) => {
-	console.log({ event });
+	const mailgun = require('mailgun-js')
+	const mg = mailgun({
+		apiKey: process.env.MAILGUN_API_KEY,
+		domain: process.env.MAILGUN_DOMAIN
+	})
 
-	// First argument is an error
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify({ boop: true })
+	const data = JSON.parse(event.body)
+	console.log(data)
+	const email = {
+		from: "Juliette Rapala <j@julietterapala.com>",
+		to: `${data.name} <${data.email}>`,
+		subject: data.subject,
+		text: data.body
+	}
+
+	mg.messages().send(email, (error, response) => {
+		callback(error, {
+			statusCode: 200,
+			body: JSON.stringify(response)
+		})
 	})
 }
