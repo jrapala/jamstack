ecafe626a114e657cc029ed4fca87740d3d50f64
Juliette Rapala <jrapala@gmail.com>
2020-09-08 14:57:00 -0500

    feat(dashboard): add route components

--- sections/dashboard/src/pages/dashboard.js
+++ sections/dashboard/src/pages/dashboard.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import Layout from '../components/layout'
+import React, { useEffect } from 'react';
+import { Router } from '@reach/router';
+import { navigate } from 'gatsby';
 
-const Dashboard = () => {
+import Layout from '../components/layout';
+import Profile from '../components/profile';
+import RouteBase from '../components/route-base';
+import RouteSecret from '../components/route-secret';
+import RouteLogin from '../components/route-login';
+
+const Dashboard = ({ location }) => {
+
+	useEffect(() => {
+		if (location.pathname.match(/^\/dashboard\/?$/)) {
+			navigate('/dashboard/login', { replace: true })
+		}
+	}, [location.pathname])
+
 	return (
 		<Layout>
-			<p>TODO: create a dashboard</p>
+			<Profile />
+			<Router>
+				<RouteBase path="/dashboard/base"/>
+				<RouteSecret path="/dashboard/secret"/>
+				<RouteLogin path="/dashboard/login"/>
+			</Router>
 		</Layout>
 	)
 }
